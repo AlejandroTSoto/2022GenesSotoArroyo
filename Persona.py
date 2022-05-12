7852c8b9037cdf289549918cc7ff13506e8f6045
ALEJANDRO TOMAS SOTO MECA <asm978@inlumine.ual.es>
2022-05-12 14:31:18 +0200

    solucion de issues

--- Persona.py
+++ Persona.py
@@ -3,7 +3,7 @@ class persona:
 
     genes = []
     #Método que genera los valores para los genes
-    def generar_Genes(self):
+    def generar_genes(self):
         for _ in range(10):
             aleatorio = secrets.randbelow(2)
             self.genes.append(aleatorio)
@@ -13,28 +13,27 @@ class persona:
         self.genes = genes
 
     #Método que mezcla a dos personas y generar una persona nueva indicando el punto de cruce
-    def cruzar_Punto(self, persona2, punto):
-        genesCruzados = []
+    def cruzar_punto(self, persona2, punto):
+        genes_cruzados = []
         #print("Punto de cruce: " + str(punto))
         for _ in range(10):
             if _ < punto:
-                genesCruzados.append(self.genes[_])
+                genes_cruzados.append(self.genes[_])
             else:
-                genesCruzados.append(persona2.genes[_])
-        return persona(genesCruzados)
+                genes_cruzados.append(persona2.genes[_])
+        return persona(genes_cruzados)
 
     #Método que muta los genes de una persona
-    def mutar_Persona(self, probabilidad):
-        genesMutados = []
-
+    def mutar_persona(self, probabilidad):
+        genes_mutados = []
         for _ in range(10):
             aleatorio = secrets.randbelow(2)
             if aleatorio <= probabilidad:
                 #print("Gen mutado: " + str(i))
                 if self.genes[_] == 0:
-                    genesMutados.append(1)
+                    genes_mutados.append(1)
                 else:
-                    genesMutados.append(0)
+                    genes_mutados.append(0)
             else:
-                genesMutados.append(self.genes[_])
-        return persona(genesMutados)
+                genes_mutados.append(self.genes[_])
+        return persona(genes_mutados)
--- Poblacion.py
+++ Poblacion.py
@@ -2,42 +2,42 @@ import secrets
 import Persona
 
 poblacion = []
-poblacionCruzadaMutada = []
+poblacion_cruzada_mutada = []
 genes = []
-personasCruzadasMutadas = []
+personas_cruzadas_mutadas = []
 
 # Método que genera una población con personas
-def generar_Poblacion(x):
+def generar_poblacion(x):
     for _ in range(x):
         persona = Persona.persona(genes)
-        persona.generar_Genes()
+        persona.generar_genes()
         poblacion.append(persona)
 
 # Método que cruza y muta a un grupo de personas según una probabilidad
-def cruzar_Mutar_Personas(grupoPersonas, probabilidad):
+def cruzar_mutar_personas(grupo_personas, probabilidad):
 
     if probabilidad < 0.5:
         print("La población se ha mutado")
-    for persona in grupoPersonas:
-        aleatorio = secrets.randbelow(len(grupoPersonas))
+    for persona in grupo_personas:
+        aleatorio = secrets.randbelow(len(grupo_personas))
         punto = secrets.randbelow(10)
-        personaCruzada = persona.cruzar_Punto(grupoPersonas[aleatorio], punto)
+        persona_cruzada = persona.cruzar_punto(grupo_personas[aleatorio], punto)
         if probabilidad < 0.5:
-            personaMutada = personaCruzada.mutar_Persona(probabilidad)
-            personasCruzadasMutadas.append(personaMutada)
+            persona_mutada = persona_cruzada.mutar_persona(probabilidad)
+            personas_cruzadas_mutadas.append(persona_mutada)
         else:
-            personasCruzadasMutadas.append(personaCruzada)
-    return personasCruzadasMutadas
+            personas_cruzadas_mutadas.append(persona_cruzada)
+    return personas_cruzadas_mutadas
 
 # Método que muestra una población de personas
-def mostrar_Poblacion(grupoPersonas):
-    for persona in grupoPersonas:
+def mostrar_poblacion(grupo_personas):
+    for persona in grupo_personas:
         print(persona.genes)
 
 #Genero una poblacion y despues esta sera cruzada y mutada
-generar_Poblacion(10)
+generar_poblacion(10)
 print("Población inicial:")
-mostrar_Poblacion(poblacion)
+mostrar_poblacion(poblacion)
 print("\n")
 print("Población cruzada y mutada: ")
-mostrar_Poblacion(cruzar_Mutar_Personas(poblacion, secrets.randbelow(2)))
+mostrar_poblacion(cruzar_mutar_personas(poblacion, secrets.randbelow(2)))
