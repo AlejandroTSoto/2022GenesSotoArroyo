59a1b4fbf542b324d04704f62e1f101493fd08d2
ALEJANDRO TOMAS SOTO MECA <asm978@inlumine.ual.es>
2022-05-12 12:59:54 +0200

    cambios poblacion

--- Poblacion.py
+++ Poblacion.py
@@ -5,11 +5,10 @@ poblacion = []
 poblacionCruzadaMutada = []
 genes = []
 personasCruzadasMutadas = []
-
+punto = secrets.randbelow(10)
 
 # Método que genera una población con personas
 def generarPoblacion(x):
-    punto = secrets.randbelow(10)
     for i in range(x):
         persona = Persona.persona(genes)
         persona.generarGenes()
